d4120027725c4c84d19e6e2cd13a8e231a9981d0
s4ngmin-9 <s4ngmin9@naver.com>
2025-08-11 21:02:50 +0900

    폴더 day2 삭제

--- day_2/schemas.py
+++
@@ -1,6 +0,0 @@
-from marshmallow import Schema, fields
-
-class BookSchema(Schema):
-    id = fields.Integer(dump_only=True)
-    title = fields.String(required=True, example="The Lord of the Rings")
-    author = fields.String(required=True, example="J.R.R. Tolkien")
